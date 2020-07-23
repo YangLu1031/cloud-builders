8bc8cdf31622f059343180dd4d347421d35ce536
David Bendory <bendory@google.com>
2020-07-23 08:12:14 -0400

    No need to make cloudbuild.yaml.

--- Beta/add-builder.sh
+++ Beta/add-builder.sh
@@ -18,29 +18,6 @@ echo '*
 echo "FROM $2
 ENTRYPOINT [\"$1\"]" > "$1/Dockerfile"
 
-echo "# In this directory, run the following command to build this build step.
-# gcloud builds submit --substitutions=_YYYYMMDD=\$(date +%Y%m%d)
-
-steps:
-- name: 'gcr.io/cloud-builders/docker'
-  args:
-  - 'build'
-  - '--tag=us-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1'
-  - '--tag=europe-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1'
-  - '--tag=asia-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1'
-  - '--tag=us-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1:\${_YYYYMMDD}'
-  - '--tag=europe-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1:\${_YYYYMMDD}'
-  - '--tag=asia-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1:\${_YYYYMMDD}'
-  - '.'
-images:
-- 'us-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1:latest'
-- 'europe-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1:latest'
-- 'asia-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1:latest'
-- 'us-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1:\${_YYYYMMDD}'
-- 'europe-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1:\${_YYYYMMDD}'
-- 'asia-docker.pkg.dev/\$PROJECT_ID/cloud-builders/$1:\${_YYYYMMDD}'
-timeout: 900s" > "$1/cloudbuild.yaml"
-
 echo "# $1
 
 This \`$1\` build step is based on the \`$2\` image supplied by TODO
