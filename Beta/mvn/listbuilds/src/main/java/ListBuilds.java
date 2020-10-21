5a0f5fab4e2c6ec0f66299eb1c0e09fd21290f89
David Bendory <bendory@google.com>
2020-10-21 15:42:12 -0400

    Listbuilds for mvn.

---
+++ Beta/mvn/listbuilds/src/main/java/ListBuilds.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import com.google.cloud.devtools.cloudbuild.v1.CloudBuildClient;
+import com.google.cloudbuild.v1.Build;
+
+
+public class ListBuilds {
+    public static void main(String[] args) {
+		int n = 0;
+		String projectId = args[0];
+		int limit = Integer.parseInt(args[1]);
+
+        try (CloudBuildClient cb = CloudBuildClient.create()) {
+            for (Build b : cb.listBuilds(projectId, "").iterateAll()) {
+                System.out.printf("Build: %s %s\n", b.getId(), b.getStatus());
+				n++;
+				if (n==limit) {
+					break;
+				}
+            }
+        } catch (IOException e) {
+			System.err.printf("ERROR: %s.\n", e);
+		}
+        System.out.printf("Listed %d builds for project %s.\n", n, projectId);
+    }
+}
