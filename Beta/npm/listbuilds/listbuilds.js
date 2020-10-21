69d17bc2b415be4d751d5410600f96f6f6e05d84
David Bendory <bendory@google.com>
2020-10-21 18:49:44 -0400

    Removed excessive comments.

--- Beta/npm/listbuilds/listbuilds.js
+++ Beta/npm/listbuilds/listbuilds.js
@@ -1,20 +1,14 @@
 'use strict';
 
-// title: List Builds
-// description: lists builds
 // usage: node list-builds.js <PROJECT_ID>
 
-// [START cloudbuild_list_buildS]
 async function listBuilds(
   projectId = 'YOUR_PROJECT_ID'
 ) {
-  // Imports the Google Cloud client library
   const {CloudBuildClient} = require('@google-cloud/cloudbuild');
 
-  // Creates a client
   const cb = new CloudBuildClient();
 
-  // What project should we list triggers for?
   const request = {
       projectId: projectId,
       pageSize: 10,
@@ -23,7 +17,6 @@ async function listBuilds(
   const [result] = await cb.listBuilds(request);
   console.table(result, ['id', 'status']);
 }
-// [END cloudbuild_list_builds]
 
 const args = process.argv.slice(2);
 listBuilds(...args).catch(console.error);
