766e9ac32c01c94e7afbfd121bdd6ce48eb323dd
David Bendory <bendory@google.com>
2020-10-21 20:36:43 -0400

    Use metadata to get projectID.

--- Beta/npm/listbuilds/listbuilds.js
+++ Beta/npm/listbuilds/listbuilds.js
@@ -1,19 +1,18 @@
 'use strict';
 
-// usage: node list-builds.js <PROJECT_ID>
+// usage: node list-builds.js
 
-async function listBuilds(
-  projectId = 'YOUR_PROJECT_ID'
-) {
+async function listBuilds() {
+  const gcpMetadata = require('gcp-metadata');
+  const projectId = await gcpMetadata.project('project-id');
+
   const {CloudBuildClient} = require('@google-cloud/cloudbuild');
-
   const cb = new CloudBuildClient();
 
   const request = {projectId};
-
   const [result] = await cb.listBuilds(request);
+
   console.table(result, ['id', 'status']);
 }
 
-const args = process.argv.slice(2);
-listBuilds(...args).catch(console.error);
+listBuilds().catch(console.error);
